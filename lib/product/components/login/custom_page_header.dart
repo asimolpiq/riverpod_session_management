92c014188377a47e328a9e77e646c8c042e13ee5
Alpay GÜROĞLU <63578350+asimolpiq@users.noreply.github.com>
2022-12-29 21:14:02 +0300

    view update

--- lib/product/components/login/custom_page_header.dart
+++ lib/product/components/login/custom_page_header.dart
@@ -16,22 +16,6 @@ class CustomPageHeader {
           width: MediaQuery.of(context).size.width,
         )),
         Positioned(
-          child: AssetManager.getPngImage(_images.left_elips.name, alignment: Alignment.topLeft),
-        ),
-        Positioned(right: 0, child: AssetManager.getPngImage(_images.right_elips.name, alignment: Alignment.topRight)),
-        // Positioned(
-        //     top: 180,
-        //     right: 0,
-        //     left: 0,
-        //     child: Text(
-        //       textAlign: TextAlign.center,
-        //       LoginStrings.login,
-        //       style: Theme.of(context).textTheme.displaySmall?.copyWith(
-        //             fontSize: 25,
-        //             color: Theme.of(context).colorScheme.surface,
-        //           ),
-        //     )),
-        Positioned(
           right: 0,
           left: 0,
           top: 100,
